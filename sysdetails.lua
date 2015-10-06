558c9b4a75cab56461b2f33f7e26ac909b4b131d
Megalegacy98 <megalegacy98@gmail.com>
2015-10-06 16:10:52 -0700

    Create sysdetails.lua

---
+++ sysdetails.lua
@@ -0,0 +1,11 @@
+firmware = System.getFirmware()
+region = System.getRegion()
+kernel = System.getKernel()
+model = System.getModel()
+language = System.getLanguage()
+color = Color.new(255,135,255)
+
+while true do
+Screen.debugPrint(0,15, .. firmware .. , color,TOP_SCREEN)
+Screen.debugPrint(0,15, .. firmware .. , color,TOP_SCREEN)
+Screen.debugPrint(0,15, .. firmware .. , color,TOP_SCREEN)
