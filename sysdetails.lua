29905ef21b4e6b47f054405dabb964a1c29e498a
Megalegacy98 <megalegacy98@gmail.com>
2015-10-06 16:21:05 -0700

    Added some additions. Untested.

--- sysdetails.lua
+++ sysdetails.lua
@@ -4,8 +4,16 @@ kernel = System.getKernel()
 model = System.getModel()
 language = System.getLanguage()
 color = Color.new(255,135,255)
+pad = Controls.read()
 
 while true do
-Screen.debugPrint(0,15, .. firmware .. , color,TOP_SCREEN)
-Screen.debugPrint(0,15, .. firmware .. , color,TOP_SCREEN)
-Screen.debugPrint(0,15, .. firmware .. , color,TOP_SCREEN)
+Screen.debugPrint(0,15,"Your firmware is: " .. firmware .. "." , color,TOP_SCREEN)
+Screen.debugPrint(0,30,"Your system region is: " .. region .. "." , color,TOP_SCREEN)
+Screen.debugPrint(0,45,"Your kernel version: " .. kernel .. ".", color,TOP_SCREEN)
+Screen.debugPrint(0,60,"Your system model: " .. model .. ".", color,TOP_SCREEN)
+Screen.debugPrint(0,75,"The language on your 3DS is: " .. language .. ".", color,TOP_SCREEN)
+Screen.debugPrint(0,125,"Press B to exit.", color,TOP_SCREEN)
+  if (Controls.check(pad,KEY_B))
+  dofile(System.currentDirectory() .. "/index.lua")
+  end
+end
