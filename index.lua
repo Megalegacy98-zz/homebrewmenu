5b6af9fb7a48dba23a26b7b4b06cc4e3eddb54b0
Megalegacy98 <megalegacy98@gmail.com>
2015-10-06 18:34:06 -0700

    Update index.lua

--- index.lua
+++ index.lua
@@ -29,7 +29,7 @@ Screen.refresh()
 Screen.clear(TOP_SCREEN)
 lol = {unselect,unselect,unselect,unselect,unselect,unselect}
 lol[y] = selected
-Screen.debugPrint(45,0, .. user .. "'s Homebrew Menu v 0.1a",unselect,TOP_SCREEN)
+Screen.debugPrint(45,0, user .. "'s Homebrew Menu v 0.1a",unselect,TOP_SCREEN)
 Screen.debugPrint(45,30,"Load Game Cartridge",lol[1],TOP_SCREEN)
 Screen.debugPrint(45,45,"Load Homebrew",lol[2],TOP_SCREEN)
 Screen.debugPrint(45,60,"Reboot System",lol[3],TOP_SCREEN)
