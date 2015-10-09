fe8871af8cb1f522484003d6e91ba4d18236619b
Megalegacy98 <megalegacy98@gmail.com>
2015-10-08 21:47:01 -0700

    fix, still need to actually return readable values

--- sysdetails.lua
+++ sysdetails.lua
@@ -7,13 +7,17 @@ color = Color.new(255,135,255)
 pad = Controls.read()
 
 while true do
+Screen.waitVblankStart()
+Screen.refresh()
+Screen.clear(TOP_SCREEN)
+Screen.clear(BOTTOM_SCREEN)
 Screen.debugPrint(0,15,"Your firmware is: " .. firmware .. "." , color,TOP_SCREEN)
 Screen.debugPrint(0,30,"Your system region is: " .. region .. "." , color,TOP_SCREEN)
 Screen.debugPrint(0,45,"Your kernel version: " .. kernel .. ".", color,TOP_SCREEN)
 Screen.debugPrint(0,60,"Your system model: " .. model .. ".", color,TOP_SCREEN)
 Screen.debugPrint(0,75,"The language on your 3DS is: " .. language .. ".", color,TOP_SCREEN)
 Screen.debugPrint(0,125,"Press B to exit.", color,TOP_SCREEN)
-  if (Controls.check(pad,KEY_B)) then
-  dofile(System.currentDirectory() .. "/index.lua")
-  end
+if (Controls.check(pad,KEY_B)) then
+dofile(System.currentDirectory() .. "/index.lua")
 end
+end 
