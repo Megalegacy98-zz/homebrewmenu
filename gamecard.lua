c3909da0475287cc8860ffc2411f02b6ab6efe8d
Megalegacy98 <megalegacy98@gmail.com>
2015-10-09 10:47:57 -0700

    Made a detection thing in case there is no gamecard... untested.

--- gamecard.lua
+++ gamecard.lua
@@ -1,8 +1,22 @@
+oldpad = Controls.read()
 while true do
-	Screen.refresh()
-	Screen.waitVblankStart()
-	Screen.clear(TOP_SCREEN)
-	Screen.debugPrint(0,0,"Loading game cartridge...",Color.new(243,192,135),TOP_SCREEN)
-	Screen.flip()
-	System.launchGamecard()
+pad = Controls.read()
+Screen.refresh()
+Screen.waitVblankStart()
+Screen.clear(TOP_SCREEN)
+Screen.debugPrint(0,0,"Loading game cartridge...",Color.new(243,192,135),TOP_SCREEN)
+System.launchGamecard()
+else
+y = 1
+end
+
+if y == 1 then
+Screen.debugPrint(0,0,"There is no game card inserted! Press B to exit.",Color.new(243,192,135),TOP_SCREEN)
+if (Controls.check(pad,KEY_B)) and not (Controls.check(oldpad,KEY_B)) then
+dofile (System.currentDirectory() .. "/index.lua")
+end
+
+Screen.flip()
+oldpad = pad
+
 end
